2578212b9ef471e6fc17c09fea0aac94b0b75812
小室敦 <komuroatsushi@komuroatsushi-no-MacBook-Pro.local>
2015-08-14 18:50:55 +0900

    [fix] 写真撮影、撮影画像をアルバムに保存する機能を追加

--- DevTest/ViewController.swift
+++ DevTest/ViewController.swift
@@ -2,8 +2,11 @@
 //  ViewController.swift
 //  sousa
 //
-//  Created by Misato Morino on 2015/08/13.
+//  Created by Misato Morino on 2015/08/14.
 //  Copyright (c) 2015年 Atsushi Komuro. All rights reserved.
+//  参考：
+//    ・https://sites.google.com/a/gclue.jp/swift-docs/ni-yinki100-ios/3-avfoundation/002-kamerano-qi-dongto-hua-xiangno-bao-cun
+//  　 ・開発のプロが教えるSwift標準ガイドブック
 //
 
 import UIKit
@@ -54,6 +57,40 @@ class ViewController: UIViewController{
         //セッション開始
         CapSession.startRunning()
         
+        //UIボタンを作成
+        let Btn = UIButton(frame: CGRectMake(0,0,120,50))
+        Btn.backgroundColor = UIColor.redColor()
+        Btn.layer.masksToBounds = true
+        Btn.setTitle("Take", forState: .Normal)
+        Btn.layer.cornerRadius = 20.0
+        Btn.layer.position = CGPoint(x: self.view.bounds.width/2, y: self.view.bounds.height-50)
+        Btn.addTarget(self, action: "onClickMyButton:", forControlEvents: .TouchUpInside)
         
+        //UIボタンをViewに追加
+        self.view.addSubview(Btn)
+    }
+    
+    //ボタンイベント
+    func onClickMyButton(sender: UIButton){
+        //ビデオ出力に接続
+        let videoConnection = ImageOut.connectionWithMediaType(AVMediaTypeVideo)
+        
+        //接続から画像を取得
+        self.ImageOut.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler:
+            {(imageDataBuffer: CMSampleBuffer?, error:NSError?)->Void in
+                if(imageDataBuffer == nil){
+                    //completion!(image:nil,error:error)
+                }
+                
+                //取得したImageのDataBufferをJpegに変換
+                let imageData: NSData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(imageDataBuffer)
+                
+                //JpegからUIImageを作成
+                let image : UIImage = UIImage(data: imageData)!
+                
+                //アルバムに追加
+                UIImageWriteToSavedPhotosAlbum(image, self, nil, nil)
+            }
+        )
     }
 }
